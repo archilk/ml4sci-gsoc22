d5891508ec8ad5ff557896224231c50243147470
Archil Srivastava <archil@ucsd.edu>
2022-07-26 18:47:34 -0700

    Added convit small

--- deeplense/constants.py
+++ deeplense/constants.py
@@ -3,5 +3,6 @@ LABELS = ['axion', 'cdm', 'no_sub']
 LABEL_MAP = {'axion': 0, 'cdm': 1, 'no_sub': 2}
 
 TIMM_IMAGE_SIZE = {
-    'vit_base_patch16_224': 224
+    'vit_base_patch16_224': 224,
+    'convit_small': 224,
 }
