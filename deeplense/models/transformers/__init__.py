7404f6eee972430310fe94cc12181d4da6bb5eef
Archil Srivastava <archil@ucsd.edu>
2022-09-16 11:32:29 +0530

    Pass dropout to transformer zoo from main file

--- deeplense/models/transformers/__init__.py
+++ deeplense/models/transformers/__init__.py
@@ -1,7 +1,7 @@
 from .cvt import CvT
 
-def get_transformer_model(name, num_classes=3):
+def get_transformer_model(name, dropout=0., num_classes=3):
     if name == 'cvt':
-        return CvT(num_classes=num_classes)
+        return CvT(num_classes=num_classes, dropout=dropout)
     else:
         return None
