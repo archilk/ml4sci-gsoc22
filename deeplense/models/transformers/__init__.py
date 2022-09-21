1612f367dac7a5b6a7d2dfe29ba77819e2629e5e
Archil Srivastava <archil@ucsd.edu>
2022-09-21 14:51:45 +0530

    Add hybrid swin model

--- deeplense/models/transformers/__init__.py
+++ deeplense/models/transformers/__init__.py
@@ -1,7 +1,10 @@
 from .cvt import CvT
+from .hybrid_swin import Hybrid
 
 def get_transformer_model(name, dropout=0., num_classes=3):
     if name == 'cvt':
         return CvT(num_classes=num_classes, dropout=dropout)
+    elif name == 'swin_hybrid':
+        return Hybrid()
     else:
         return None
